0becbcdeae7b941968ad89134f1f21ae32dd04f7
jooooonj <lee22424205@naver.com>
2023-03-24 17:39:14 +0900

    finished login, logout functionality using spring security

--- src/main/java/com/myboard/sbb/domain/user/controller/UserController.java
+++ src/main/java/com/myboard/sbb/domain/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.myboard.sbb.domain.user.controller;
 
+import com.myboard.sbb.domain.user.entity.SiteUser;
 import com.myboard.sbb.domain.user.form.SignupForm;
 import com.myboard.sbb.domain.user.service.UserService;
 import jakarta.validation.Valid;
@@ -11,6 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
@@ -19,13 +21,13 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/signup")
-    public String signup(SignupForm signupForm){
+    public String signup(SignupForm signupForm) {
         return "user/signup";
     }
 
     @PostMapping("/signup")
-    public String signup(@Valid SignupForm signupForm, BindingResult bindingResult, Model model){
-        if(bindingResult.hasErrors())
+    public String signup(@Valid SignupForm signupForm, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors())
             return "user/signup";
 
         if (!signupForm.getPassword().equals(signupForm.getPassword2())) {
@@ -35,16 +37,26 @@ public class UserController {
 
         try {
             userService.register(signupForm);
-        }catch(DataIntegrityViolationException e) {
+        } catch (DataIntegrityViolationException e) {
             e.printStackTrace();
             bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
             return "user/signup";
-        }catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             bindingResult.reject("signupFailed", e.getMessage());
             return "user/signup";
         }
         return "redirect:/";
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "user/login";
+    }
+
+
+
+
+    //Spring Security가 Post 알아서 하니까 구현할 필요 없다.
 
 }
